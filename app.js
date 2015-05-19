66d407a010bbad755a955efeb586f03cb88b6298
adelaidesmith <adelaidesmith@seattleacademy.org>
2015-05-19 08:51:25 -0700

    add index.js app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var geometry = require('./index.js');
+console.log( 'The area of a circle of radius 4 is '
+           + geometry.circlearea(4));
+console.log('The circumference of a circle with radius 4 is', geometry.circlecircumference(4));
+
+console.log('The area of a square with side 4 is', geometry.squarearea(4));
+console.log('The perimeter of a square with side 4 is', geometry.squareperimeter(4));
---
+++ index.js
@@ -0,0 +1,17 @@
+var PI = Math.PI;
+
+exports.circlearea = function (r) {
+  return PI * r * r;
+};
+
+exports.circlecircumference = function (r) {
+  return 2 * PI * r;
+};
+
+exports.squarearea = function (s) {
+  return s * s;
+};
+
+exports.squareperimeter = function (s) {
+  return s * 4;
+};
